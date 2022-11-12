fd80ce547122794c2dee448d77e534188ad576ff
Len Ganley <lenjganley@gmail.com>
2022-11-11 23:07:17 -0500

    Gallery Page autogenerates HTML based on const albumSize

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function generateAlbum(pics) {
+    let albumCode = "";
+    for (i = 1; i <= pics; i++) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        albumCode +=
+        `<div class="col-4 col-lg-3 d-flex justify-content-center my-5">
+            <a href="img/screenshots/${i}.png" data-lightbox="screenshots">
+                <img class="img-thumbnail dropShadow" src="img/screenshots/t${i}.png" alt="thumbnail${i}">
+            </a>
+        </div> `
+    }
+    return albumCode;
+}
